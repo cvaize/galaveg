933d4ba9b27a3994b3e8f28e2932af46130b37ba
cvaize <cvaize@gmail.com>
2025-11-01 15:42:49 +0500

    Init

---
+++ backend/database/migrations/migrations.go
@@ -0,0 +1,24 @@
+package migrations
+
+import (
+	"database/sql"
+)
+
+type Migration struct {
+	Uuid string
+	Up   func(db *sql.DB) error
+	Down func(db *sql.DB) error
+}
+
+var Migrations = []Migration{
+	Migration{
+		Uuid: "0001_01_01_000000_create_users_table",
+		Up:   CreateUsersTable00010101000000Up,
+		Down: CreateUsersTable00010101000000Down,
+	},
+	Migration{
+		Uuid: "2025_10_31_115348_create_roles_table",
+		Up:   CreateRolesTable20251031115348Up,
+		Down: CreateRolesTable20251031115348Down,
+	},
+}
