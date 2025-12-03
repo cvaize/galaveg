1bdf53bff7cef437f87bb6be151d3418967a8910
cvaize <cvaize@gmail.com>
2025-12-03 17:57:45 +0500

    Init

--- internal/bootstrap/http/handlers/http/controllers/api/v1/controller.go
+++ internal/bootstrap/http/handlers/http/controllers/api/v1/controller.go
@@ -2,7 +2,6 @@ package v1
 
 import (
 	"galaveg/internal/bootstrap/http/context"
-	"galaveg/pkg/debug"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -16,11 +15,10 @@ func NewController(ctx *context.Context) Controller {
 }
 
 func (ctr *Controller) Index(c *gin.Context) {
-	f := make(map[string]interface{})
-	//f["id"] = 2
-	dto, err := ctr.ctx.Services.Roles.All(f, "", make([]string, 0))
-	debug.Dump(dto)
-	debug.Dump(err)
+	//f := make(map[string]interface{})
+	//dto, err := ctr.ctx.Services.Roles.All(f, "", make([]string, 0))
+	//debug.Dump(dto)
+	//debug.Dump(err)
 	c.JSON(http.StatusOK, gin.H{
 		"status": "ok",
 	})
--- internal/modules/roles/service.go
+++ internal/modules/roles/service.go
@@ -12,14 +12,14 @@ func NewService(dbRepo DbRepo) (*ServiceImpl, *errors.Error) {
 	return &ServiceImpl{dbRepo}, nil
 }
 
-func (r *ServiceImpl) AllIds(filters map[string]interface{}, orderBy string) ([]ID, error) {
-	return r.dbRepo.AllIds(filters, orderBy)
-}
-
-func (r *ServiceImpl) All(filters map[string]interface{}, orderBy string, columns []string) ([]*RoleDto, error) {
-	return r.dbRepo.All(filters, orderBy, columns)
-}
-
-func (r *ServiceImpl) First(filters map[string]interface{}, columns []string) (*RoleDto, error) {
-	return r.dbRepo.First(filters, columns)
-}
+//func (r *ServiceImpl) AllIds(filters map[string]interface{}, orderBy string) ([]ID, error) {
+//	return r.dbRepo.AllIds(filters, orderBy)
+//}
+//
+//func (r *ServiceImpl) All(filters map[string]interface{}, orderBy string, columns []string) ([]*RoleDto, error) {
+//	return r.dbRepo.All(filters, orderBy, columns)
+//}
+//
+//func (r *ServiceImpl) First(filters map[string]interface{}, columns []string) (*RoleDto, error) {
+//	return r.dbRepo.First(filters, columns)
+//}
